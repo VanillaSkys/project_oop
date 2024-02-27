4382906ba68f1a4e7d67819144de71916b0354d7
VanillaSkys <filllovefamily@gmail.com>
2024-02-27 21:59:42 +0700

    update

--- server/src/config/EnvConfig.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseSettings
-
-class EnvConfig(BaseSettings):
-    HOST: str = "localhost"
-    PORT: int = 8000
--- server/src/controller/MainController.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from src.controller.UserController import UserController
-class MainController:
-    def __init__(self):
-        self.router = APIRouter()
-        self.register_controllers()
-
-    def register_controllers(self):
-        self.router.include_router(UserController().router, prefix="/user")
