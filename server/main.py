131a04c4c2c779019667939c544aef3c4a5bd8f0
VanillaSkys <filllovefamily@gmail.com>
2024-02-27 22:24:21 +0700

    update

---
+++ server/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/public", StaticFiles(directory="public"), name="public")
+# Controller
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
