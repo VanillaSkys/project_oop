58f41ef0b7baab69b5b648ca98ec28b55ef82404
TroJanBoi <86398888+TroJanBoi@users.noreply.github.com>
2024-03-04 13:31:21 +0700

    cartoonPage

--- client/src/pages/CartoonPage.jsx
+++ client/src/pages/CartoonPage.jsx
@@ -1,8 +1,17 @@
 function CartoonPage()
 {
     return (
-        <div className="p-4 h-screen flex items-center justify-center bg-[url(../../public/assets/image/solo/bg-1.jpg)]">
-
+        <div className="h-screen flex items-center justify-center">
+            <img src="https://th-a.kakaopagecdn.com/P/C/48/bg/2x/2d678b69-787d-4a80-a854-32ed5d4f630f.jpg" alt="" width={"100%"} />
+            <div className="grid grid-rows-7">
+				<div>Login</div>
+				<div>Websiter</div>
+				<div>03</div>
+				<div>04</div>
+				<div>05</div>
+				<div>06</div>
+				<div>07</div>
+			</div>
         </div>
     );
 }
