bebc03ace0119cb6f771b04d5a8d75f8eb69ba02
TroJanBoi <86398888+TroJanBoi@users.noreply.github.com>
2024-03-04 21:11:13 +0700

    done

--- client/src/pages/ChapterPage.jsx
+++ client/src/pages/ChapterPage.jsx
@@ -1,17 +1,32 @@
 function ChapterPage()
 {
 	return(
-		<div className="bg-white justify-center item-center">
-			<div class="grid grid-rows-4 grid-flow-col gap-4">
-				<div><img className="absolute object-contain m-auto w-full h-full m-0" src="https://ali.xn--s3cx7a.com/B/img/SoloLeveling-1_001.jpg" alt=""></img></div>
-				<div><img className="absolute object-contain m-auto w-full h-full m-0" src="https://ali.xn--s3cx7a.com/B/img/SoloLeveling-1_002.jpg" data-server="Server1" data-ll-status="loaded"/></div>
+		<div className="bg-white w-full flex justify-center">
+			<div className="w-[630px]">
+				<img className="object-contain" src="https://ali.xn--s3cx7a.com/B/img/SoloLeveling-1_001.jpg" alt=""></img>
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-1.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-2.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-3.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-4.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-5.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-6.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-7.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-8.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-9.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-10.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-11.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-12.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-13.jpg" alt="" />
+				<img className="object-contain" src="https://www.up-manga.com/wp-content/uploads/2024/01/Solo-Leveling-ep1-14.jpg" alt="" />
 			</div>
 		</div>
 	);
 }
 export default ChapterPage
 
-{/* <div className="col-span-2">
-					<img className="absolute object-contain m-auto  w-full h-full m-0" src="https://ali.xn--s3cx7a.com/B/img/SoloLeveling-1_001.jpg" alt=""></img>
-					<img className="absolute w-full block max-h-full max-w-full object-contain  inset-0 m-auto pointer-events-none w-full h-full m-0" src="https://ali.xn--s3cx7a.com/B/img/SoloLeveling-1_002.jpg" data-server="Server1" data-ll-status="loaded"/>
-				</div> */}
+{/* 
+<div className="col-span-2">
+	<img className="absolute object-contain m-auto  w-full h-full m-0" src="https://ali.xn--s3cx7a.com/B/img/SoloLeveling-1_001.jpg" alt=""></img>
+	<img className="absolute w-full block max-h-full max-w-full object-contain  inset-0 m-auto pointer-events-none w-full h-full m-0" src="https://ali.xn--s3cx7a.com/B/img/SoloLeveling-1_002.jpg" data-server="Server1" data-ll-status="loaded"/>
+</div> 
+*/}
