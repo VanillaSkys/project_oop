bfc81549a620ff5ce44860fa3f816d546148ba95
HaMaGoD <boss.jilawat@gmail.com>
2024-03-09 21:30:07 +0700

    .

--- bot/data/Cartoon.py
+++ bot/data/Cartoon.py
@@ -2,7 +2,7 @@
     {
         "name" : "Solo Leveling",
         "author" : "Chugong",
-        "category"  : ["Action"],
+        "category"  : ["action"],
         "image_cartoon" : "/cartoon/Solo Leveling/cartoon.png",
         "image_bg" : "/cartoon/Solo Leveling/bg.png",
         "image_mains" : "/cartoon/Solo Leveling//main.png"
