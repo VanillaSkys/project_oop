2f0d376e826ac00c907fb85e6326c691dea3e554
VanillaSkys <filllovefamily@gmail.com>
2024-03-09 16:42:21 +0700

    update

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -39,8 +39,8 @@ const handleSubmit = async (e) => {
       <div>
 					<Link to="/" className="text-center">
 						{/* <img src="../../public/assets/image/left-arrow.png" className="object-contain" height={"10%"} width={"10%"} alt="" /> */}
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="ml-2 w-10 h-10">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="ml-2 w-10 h-10">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
 						</svg>
 					</Link>
 				</div>
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,80 +1,111 @@
-import {Swiper, SwiperSlide} from 'swiper/react'
-import 'swiper/css'
-import {motion, useMotionValue, useSpring, useTransform} from 'framer-motion'
-import React, { useRef, useState, useEffect } from "react";
-import { FiMousePointer } from 'react-icons/fi';
-import DrawOutlineButton from '../components/DrawOutlineButton';
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import DrawOutlineButton from "../components/DrawOutlineButton";
 
 function handleClick() {
+  // document.body.button.zoom = "500%";
 
-	// document.body.button.zoom = "500%";
-
-	window.location.href='/Latest';
-  }
+  window.location.href = "/Latest";
+}
 
 const ROTATION_RANGE = 32.5;
 const HALF_ROTATION_RANGE = 32.5 / 2;
 
 function LandingPage() {
-		// window.location.href = "/Latest"
-	const ref = useRef(null);
-	
-	const [rotateX, setRotateX] = useState(0);
-  	const [rotateY, setRotateY] = useState(0);
+  // window.location.href = "/Latest"
+  const ref = useRef(null);
 
-	  const handleMouseMove = (e) => {
-		if (!ref.current) return;
-	
-		const rect = ref.current.getBoundingClientRect();
-	
-		const width = rect.width;
-		const height = rect.height;
-	
-		const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
-		const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
-	
-		const rY = mouseX / width - HALF_ROTATION_RANGE;
-		const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
-	
-		setRotateX(rX);
-		setRotateY(rY);
-	};
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
 
-	const handleMouseLeave = () => {
-		if (!ref.current) return;
-		setRotateX(0);
-		setRotateY(0);
-	};
-	
-	return (
-		<div className="h-screen bg-gray-200">
-			<div className="w-[950px] mx-auto pt-24 px-2 grid grid-cols-4 gap-1">
-				<div className="h-[550px] flex items-between justify-between col-span-4">
-					<div style={{backgroundColor:'#020617'}} className=' w-full text-center rounded-l-lg drop-shadow-2xl shadow-lg shadow-violet-500'>
-						<DrawOutlineButton onClick={handleClick}>Web site</DrawOutlineButton>
-					</div>
-					<div className='text-center w-[550px] object-cover rounded-r-lg bg-cover bg-center '>
-						<motion.div className="relative w-[550px] drop-shadow-2xl shadow-lg shadow-violet-500 rounded-r-xl" ref={ref} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{backgroundImage:'url(../../public/assets/image/bg-2.jpg)', transformStyle: "preserve-3d",  height:'550px'}} animate={{ rotateX, rotateY, }}>
-							<div style={{ transform: "translateZ(75px)", transformStyle: "preserve-3d",}}>
-								<img src="https://th-a.kakaopagecdn.com/P/C/353/t2/2x/cc4fa235-5cd8-4a43-ae83-7d930e188713.png" className='absolute' alt="" />
-								<video style={{width:'650px', height:'608px'}} className='absolute ml-16 inset-4 grid place-content-center'  autoPlay muted loop>
-									<source  src=" https://th-a.kakaopagecdn.com/P/C/353/c1a/a124957b-5d43-42b0-ac3c-571844949405.webm" className='w-[400px] mx-auto' type="video/webm" />
-								</video>
-							</div>
-						</motion.div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  const handleMouseMove = (e) => {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+    const rY = mouseX / width - HALF_ROTATION_RANGE;
+    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
+
+    setRotateX(rX);
+    setRotateY(rY);
+  };
+
+  const handleMouseLeave = () => {
+    if (!ref.current) return;
+    setRotateX(0);
+    setRotateY(0);
+  };
+
+  return (
+    <div className="h-screen bg-gray-200">
+      <div className="w-[950px] mx-auto pt-24 px-2 grid grid-cols-4 gap-1">
+        <div className="h-[550px] flex items-between justify-between col-span-4">
+          <div
+            style={{ backgroundColor: "#020617" }}
+            className=" w-full text-center rounded-l-lg drop-shadow-2xl shadow-lg shadow-violet-500"
+          >
+            <DrawOutlineButton onClick={handleClick}>
+              Web site
+            </DrawOutlineButton>
+          </div>
+          <div className="text-center w-[550px] object-cover rounded-r-lg bg-cover bg-center ">
+            <motion.div
+              className="relative w-[550px] drop-shadow-2xl shadow-lg shadow-violet-500 rounded-r-xl"
+              ref={ref}
+              onMouseMove={handleMouseMove}
+              onMouseLeave={handleMouseLeave}
+              style={{
+                backgroundImage: "url(../../public/assets/image/bg-2.jpg)",
+                transformStyle: "preserve-3d",
+                height: "550px",
+              }}
+              animate={{ rotateX, rotateY }}
+            >
+              <div
+                style={{
+                  transform: "translateZ(75px)",
+                  transformStyle: "preserve-3d",
+                }}
+              >
+                <img
+                  src="https://th-a.kakaopagecdn.com/P/C/353/t2/2x/cc4fa235-5cd8-4a43-ae83-7d930e188713.png"
+                  className="absolute"
+                  alt=""
+                />
+                <video
+                  style={{ width: "650px", height: "608px" }}
+                  className="absolute ml-16 inset-4 grid place-content-center"
+                  autoPlay
+                  muted
+                  loop
+                >
+                  <source
+                    src=" https://th-a.kakaopagecdn.com/P/C/353/c1a/a124957b-5d43-42b0-ac3c-571844949405.webm"
+                    className="w-[400px] mx-auto"
+                    type="video/webm"
+                  />
+                </video>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
 
-								
 // https://th-a.kakaopagecdn.com/P/C/353/c1a/a124957b-5d43-42b0-ac3c-571844949405.webm
 
-{/* <Swiper spaceBetween={20} slidesPerView={1} autoplay={{ delay: 1000 }} loop>
+{
+  /* <Swiper spaceBetween={20} slidesPerView={1} autoplay={{ delay: 1000 }} loop>
 	<SwiperSlide className='text-center  object-contain bg-left rounded-r-lg'>
 
 	</SwiperSlide>
@@ -82,7 +113,8 @@ export default LandingPage
 		<img src="https://th-a.kakaopagecdn.com/P/C/48/t2/2x/82c39060-e9e6-400b-8d03-3505e552e172.png" className='absolute drop-shadow-2xl' alt="" />
 		<motion.img src="../../public/assets/image/solo/sung-jin-woo.png" className='mt-32 w-[500px] mx-auto' alt="" />
 	</SwiperSlide>
-</Swiper> */}
+</Swiper> */
+}
 
 // hover:scale-[1000%] hover:transition hover:duration-1000
 
@@ -94,24 +126,26 @@ export default LandingPage
 //   <source src="https://th-a.kakaopagecdn.com/P/C/769/c1a/b7b52d87-e241-4172-8e64-0e2f90c54abb.webm" type="video/webm" />
 // </video>
 // <div className=''>
-//   <Link to="/Lastest"><button className='absolute w-34 p-2 text-3xl rounded-md mt-28 ml-a outline hover:bg-amber-200 text-white '>Web site</button></Link> 
+//   <Link to="/Lastest"><button className='absolute w-34 p-2 text-3xl rounded-md mt-28 ml-a outline hover:bg-amber-200 text-white '>Web site</button></Link>
 // </div>
 
 // </motion.div >
 
-
-{/* <Swiper spaceBetween={50} slidesPerView={1} autoplay={{ delay: 1000 }} loop={true}>
+{
+  /* <Swiper spaceBetween={50} slidesPerView={1} autoplay={{ delay: 1000 }} loop={true}>
 <SwiperSlide>
 	<img src="../../public/assets/image/landingPage-1.png" class="absolute object-cover w-[1920px] h-[737px]" />
 </SwiperSlide>
 <SwiperSlide>
 	<img src="../../public/assets/image/landingPage-2.jpg" class="object-cover w-[1920px] h-[737px] ..." />
 </SwiperSlide>
-</Swiper>	 */}
+</Swiper>	 */
+}
 
-
-{/* <div className="flex flex-col itmes-center justify-center">
+{
+  /* <div className="flex flex-col itmes-center justify-center">
 	<div style={{marginLeft:'68%', marginTop:'30%'}}>
 		<Link to="/Lastest"><button className='absolute text-white w-72	 outline p-3'>Web Site</button></Link>
 	</div>
-</div> */}
+</div> */
+}
