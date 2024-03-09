f87decf100770f5191e64374daf94a57aa0e3ef9
VanillaSkys <filllovefamily@gmail.com>
2024-03-09 22:04:37 +0700

    update

--- bot/data/Cartoon.py
+++ bot/data/Cartoon.py
@@ -1,11 +1,11 @@
-[
+cartoon_data = [
     {
         "name" : "Solo Leveling",
         "author" : "Chugong",
         "category"  : ["action"],
         "image_cartoon" : "/cartoon/Solo Leveling/cartoon.png",
         "image_bg" : "/cartoon/Solo Leveling/bg.png",
-        "image_mains" : "/cartoon/Solo Leveling//main.png"
+        "image_main" : "/cartoon/Solo Leveling/main.png"
     }
     
 ]
