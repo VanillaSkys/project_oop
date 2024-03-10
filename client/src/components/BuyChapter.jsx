03191e7fbee7c1990d04c3c125a392c731981faa
VanillaSkys <filllovefamily@gmail.com>
2024-03-11 02:23:12 +0700

    Update BuyChapter.jsx

--- client/src/components/BuyChapter.jsx
+++ client/src/components/BuyChapter.jsx
@@ -1,38 +1,37 @@
-import axios from "axios"
+import axios from "axios";
 import { AnimatePresence, motion } from "framer-motion";
 import { FiAlertCircle } from "react-icons/fi";
-import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 function SpringModal({ isOpen, setIsOpen, cartoon_id, chapter_id }) {
+  const { chapter, cartoon } = useParams();
+  // console.log(chapter.split('_'))
   async function Buy() {
     try {
-
-      await axios.post('/api/buy_chapter', {
-        username: localStorage.getItem('user'),
+      await axios.post("/api/buy_chapter", {
+        username: localStorage.getItem("user"),
         cartoon_id: cartoon_id,
-        chapter_id: chapter_id
-      })
-      setIsOpen(false)
-      location.reload(false)
+        chapter_id: chapter_id,
+      });
+      setIsOpen(false);
+      location.reload(false);
     } catch (error) {
       if (error.response && error.response.status === 417) {
-        if (error.response.data.error === 'coin') {
-          alert("Term Cash")
-          setIsOpen(false)
-          location.href = '/payment'
+        if (error.response.data.error === "coin") {
+          alert("Term Cash");
+          setIsOpen(false);
+          location.href = "/payment";
         } else {
-          alert("Login")
-          setIsOpen(false)
-          location.href = '/login'
+          alert("Login");
+          setIsOpen(false);
+          location.href = "/login";
         }
       } else {
         console.log("Error.");
       }
-      }
-  // setQrcode(res.data.image)
-  
+    }
+    // setQrcode(res.data.image)
   }
-
   return (
     <AnimatePresence>
       {isOpen && (
@@ -53,8 +52,19 @@ function SpringModal({ isOpen, setIsOpen, cartoon_id, chapter_id }) {
             <FiAlertCircle className="text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24" />
             <div className="relative z-10">
               <div className="bg-white w-16 h-16 mb-2 rounded-full text-3xl text-indigo-600 grid place-items-center mx-auto">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
                 </svg>
               </div>
               <h3 className="text-3xl font-bold text-center mb-2">
@@ -64,12 +74,31 @@ function SpringModal({ isOpen, setIsOpen, cartoon_id, chapter_id }) {
                 หลังจากทำการซื้อตอนแล้วจะไม่สามารถขอคืนเงินได้
               </p>
               <div className="flex gap-2">
-                <button
-                  onClick={() => setIsOpen(false)}
-                  className="bg-transparent hover:bg-white/10 transition-colors text-white font-semibold w-full py-2 rounded"
-                >
-                  ไม่ซื้อ
-                </button>
+                {chapter === undefined ? (
+                  <button
+                    onClick={() => setIsOpen(false)}
+                    className="bg-transparent hover:bg-white/10 transition-colors text-white font-semibold w-full py-2 rounded"
+                  >
+                    ไม่ซื้อ
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      setIsOpen(false);
+                      window.location.href =
+                        Number(chapter.split("_")[0]) > 1
+                          ? `/cartoon/${cartoon}/${
+                              Number(chapter.split("_")[0]) - 1
+                            }_${chapter.split("_")[1]}`
+                          : `/cartoon/${cartoon}``${
+                              Number(chapter.split("_")[0]) > 1
+                            }`;
+                    }}
+                    className="bg-transparent hover:bg-white/10 transition-colors text-white font-semibold w-full py-2 rounded"
+                  >
+                    ไม่ซื้อ
+                  </button>
+                )}
                 <button
                   onClick={() => Buy()}
                   className="bg-white hover:opacity-90 transition-opacity text-indigo-600 font-semibold w-full py-2 rounded"
@@ -83,8 +112,14 @@ function SpringModal({ isOpen, setIsOpen, cartoon_id, chapter_id }) {
       )}
     </AnimatePresence>
   );
-};
-
+}
+// to={
+//   Number(chapter.split("_")[0]) > 1
+//     ? `/cartoon/${cartoon}/${
+//         Number(chapter.split("_")[0]) - 1
+//       }_${chapter.split("_")[1]}`
+//     : `/cartoon/${cartoon}`
+// }
 export default SpringModal;
 
 // const ExampleWrapper = () => {
@@ -102,14 +137,10 @@ export default SpringModal;
 //   );
 // };
 
-
-
-
-
-    // async function Buy() {
-    //     axios.post('/api/buy_chapter', {
-    //         username: localStorage.getItem('user'),
-    //         cartoon_id: ,
-    //         chapter_id:
-    //     })
-    // }
+// async function Buy() {
+//     axios.post('/api/buy_chapter', {
+//         username: localStorage.getItem('user'),
+//         cartoon_id: ,
+//         chapter_id:
+//     })
+// }
