4fcce132b9361b7e800450eca231a34c7840014e
HaMaGoD <boss.jilawat@gmail.com>
2024-03-10 15:54:45 +0700

    .

--- bot/Author.py
+++ bot/Author.py
@@ -1,10 +1,10 @@
 import requests
-from data import author
+from data_cartoon.author import author_data
 
 class Author:
     def __init__(self, url) -> None:
         self.__url = url
-        self.__data = author.athor_adta
+        self.__data = author_data
     
     def add(self):
         for data in self.__data:
