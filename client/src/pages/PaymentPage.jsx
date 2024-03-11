3500e54e80f1fed6d0199e90f45201abe81c41da
TroJanBoi <86398888+TroJanBoi@users.noreply.github.com>
2024-03-11 10:30:04 +0700

    update

--- client/src/pages/PaymentPage.jsx
+++ client/src/pages/PaymentPage.jsx
@@ -36,13 +36,13 @@ function PaymentPage() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="mr-2 w-10 h-10"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M6 18 18 6M6 6l12 12"
                 />
               </svg>
